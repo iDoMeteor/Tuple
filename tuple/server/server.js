443ab77fd4e62d886ba1a52dd0519f17a83224ed
Brendan Stern <bsstern@gmail.com>
2015-07-03 22:33:51 -0400

    started a meteor app to test out reactivity

---
+++ tuple/server/server.js
@@ -0,0 +1,175 @@
+//this is essentially what autopublish does, may not be required
+// if (Meteor.isServer) {
+//   Meteor.publish("tasks", function () {
+//     return Tasks.find();
+//   });
+// }
+
+Meteor.startup(function () {
+  if (Players.find().count() === 0) {
+    var names = ["Ada Lovelace", "Grace Hopper", "Marie Curie",
+                 "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
+    _.each(names, function (name) {
+      Players.insert({
+        name: name,
+        score: Math.floor(Random.fraction() * 10) * 5
+      });
+    });
+  }
+});
+
+
+
+
+//
+// var http = require('http')
+//   , fs = require('fs')
+//   , io = require('socket.io')
+//   , connect = require('connect')
+//   , gzip = require('gzippo')
+//   , nowww = require('connect-no-www')
+//   , ams = require('ams')
+//   , Game = require('./game')
+//   , server
+//   , games = {}
+//   , latestPublicGame
+//   , clientDir = __dirname + '/client'
+//   , publicDir = __dirname + '/public'
+//   , depsDir = __dirname + '/deps'
+//   , prod = process.env.NODE_ENV === 'production';
+//
+// buildStaticFiles();
+//
+// function niceifyURL(req, res, next){
+//   if (/^\/game\/public/.exec(req.url)) {
+//     res.writeHead(302, {
+//       'Location': '/game/#!/' + getLatestPublicGame().hash
+//     });
+//     return res.end();
+//   }
+//   if (/^\/game$/.exec(req.url)) {
+//     res.writeHead(301, { 'Location': '/game/' });
+//     return res.end();
+//   }
+//   if (/^\/game\//.exec(req.url)) {
+//     req.url = '/game.html';
+//   } else if (/^\/about/.exec(req.url)) {
+//     req.url = '/about.html';
+//   } else if (/^\/help/.exec(req.url)) {
+//     req.url = '/help.html';
+//   } else if (/^\/?$/.exec(req.url)) {
+//     req.url = '/index.html';
+//   }
+//   return next();
+// }
+//
+// server = connect.createServer(
+//     connect.logger(':status :remote-addr :url in :response-timems')
+//   , nowww()
+//   , niceifyURL
+//   , gzip.staticGzip(publicDir, {
+//         matchType: /text|javascript/
+//       , maxAge: prod ? 86400000 : 0
+//     })
+//   , gzip.staticGzip(publicDir + '/perm', {
+//         matchType: /image|font/
+//       , maxAge: prod ? 604800000 : 0
+//     })
+// );
+//
+// server.listen(prod ? 80 : 8000);
+//
+// io = io.listen(server);
+// io.configure('production', function() {
+//   io.enable('browser client minification');  // send minified client
+//   io.enable('browser client etag');          // apply etag caching logic based on version number
+//   io.enable('browser client gzip');          // gzip the file
+//   io.set('log level', 1);                    // reduce logging
+//   io.set('transports', [                     // enable all transports (optional if you want flashsocket)
+//       'websocket'
+//     , 'flashsocket'
+//     , 'htmlfile'
+//     , 'xhr-polling'
+//     , 'jsonp-polling'
+//   ]);
+// });
+//
+// function getUnusedHash() {
+//   do { var hash = randString(4); } while (hash in games);
+//   return hash;
+// }
+// function getGame(hash) {
+//   if (hash && hash in games) return games[hash];
+//   hash = getUnusedHash();
+//   return (games[hash] = new Game(io, hash));
+// }
+//
+// function getLatestPublicGame() {
+//   if (!latestPublicGame ||
+//     latestPublicGame.started ||
+//     !(latestPublicGame.hash in games))
+//   {
+//     var hash = getUnusedHash();
+//     latestPublicGame = games[hash] = new Game(io, hash, 3);
+//   }
+//   return latestPublicGame;
+// }
+//
+// io.sockets.on('connection', function(socket){
+//   var game = null;
+//   socket.on('init', function(message){
+//     console.log('connecting socket ' + socket.id);
+//     game = getGame(message.game);
+//     game.registerClient(socket, message.sess);
+//     (game.handleClientMessage('init', socket)).call(game, message);
+//     if (message.game !== game.hash) socket.emit('setHash', game.hash);
+//   });
+//
+//   socket.on('disconnect', function() {
+//     if (!game) return;
+//     var hash = game.hash;
+//     game.unregisterClient(socket, function gameOver() {
+//       console.log('gameover called');
+//       delete games[hash];
+//     });
+//     game = null;
+//   });
+// });
+//
+// var CHARSET = ['A','C','D','E','F','G','H','J','K','L','M','N','P','Q','R','T','V','W','X','Y','Z'];
+//
+// function randString(size) {
+//   var ret = "";
+//   while (size-- > 0) {
+//     ret += CHARSET[Math.floor(Math.random() * CHARSET.length)];
+//   }
+//   return ret;
+// }
+//
+// function buildStaticFiles() {
+//   var options = {
+//     uglifyjs: prod,
+//     jstransport: false,
+//     cssabspath: false,
+//     cssdataimg: false,
+//     texttransport: false
+//   };
+//   ams.build
+//     .create(publicDir)
+//     .add(depsDir + '/JSON-js/json2.js')
+//     .add(clientDir + '/util.js')
+//     .add(depsDir + '/jquery-bbq/jquery.ba-bbq.js')
+//     .add(depsDir + '/jquery.transform.js/jquery.transform.light.js')
+//     .add(clientDir + '/client.js')
+//     .combine({js: 'client.js'})
+//     .process(options)
+//     .write(publicDir)
+//   .end();
+//   ams.build
+//     .create(publicDir)
+//     .add(clientDir + '/style.css')
+//     .add(depsDir + '/headjs/src/load.js')
+//     .process(options)
+//     .write(publicDir)
+//   .end()
+// }
