d1a39c43c55e73a055cbba7f2dff53051a69a9aa
Brendan Stern <bsstern@gmail.com>
2015-07-04 23:11:00 -0400

    added bootcards for a better gameboard, also fixed startup zombie cards

--- tuple/server/server.js
+++ tuple/server/server.js
@@ -1,15 +1,15 @@
 Meteor.startup(function() {
-  // if (Cards.find().count() === 0) {
+  Cards.remove({});
+   if (Cards.find().count() === 0) {
   var names = ["Card 1", "Card 2", "Card 3", "Card 4", "Card 5", "Card 6", "Card 7", "Card 8", "Card 9", "Card 10", "Card 11", "Card 12"];
   _.each(names, function(name) {
     Cards.insert({
-      name: name,
-      score: Math.floor(Random.fraction() * 10) * 5
+      name: name
+      // score: Math.floor(Random.fraction() * 10) * 5
     });
   });
-  //  }
+    }
 });
-
 
 
 
