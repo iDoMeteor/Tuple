5440c98402f5e3334e964a3f741472b216b122b9
Brendan Stern <bsstern@gmail.com>
2015-07-06 14:53:57 -0400

    limited the number of cards returned from the deck

--- client/client.js
+++ client/client.js
@@ -1,37 +1,36 @@
-if (Meteor.isClient) {
-  Template.board.helpers({
-    cards: function () {
-      return Cards.find({}, {
-        sort: {
-          name: 1
-        }
-      });
-    },
-    selectedName: function () {
-      var card = Cards.findOne(Session.get("selectedCard"));
-      return card && card.name;
-    }
-  });
+Template.board.helpers({
+  cards: function () {
+    return Cards.find({}, {
+      sort: {
+        count: -1
+      },
+      limit: 12
+    })
+  },
+  selectedCard: function () {
+    var card = Cards.findOne(Session.get("selectedCard"));
+    return card && card.name;
+  }
+});
 
-  Template.board.events({
-    'click .inc': function () {
-      Cards.update(Session.get("selectedCard"), {
-        $inc: {
-          score: 5
-        }
-      });
-    }
-  });
+Template.board.events({
+  'click .inc': function () {
+    Cards.update(Session.get("selectedCard"), {
+      $inc: {
+        score: 5
+      }
+    });
+  }
+});
 
-  Template.card.helpers({
-    selected: function () {
-      return Session.equals("selectedCards", this._id) ? "selected" : '';
-    }
-  });
+Template.card.helpers({
+  selected: function () {
+    return Session.equals("selectedCards", this._id) ? "selected" : '';
+  }
+});
 
-  Template.card.events({
-    'click': function () {
-      Session.set("selectedCard", this._id);
-    }
-  });
-};
+Template.card.events({
+  'click': function () {
+    Session.set("selectedCard", this._id);
+  }
+});
