b212d08fdc982a33351928eaa2073d86012f096a
Brendan Stern <bsstern@gmail.com>
2015-07-06 18:01:43 -0400

    added attributes for color, shape, quantity, and fill to the Card collection object

--- server/server.js
+++ server/server.js
@@ -5,8 +5,40 @@ Meteor.startup(function () {
     _.each(names, function (name) {
       Cards.insert({
         name: name,
-        score: 0
+        score: 0,
+        color: 'x' | 'y' | 'z',
+        shape: 'x' | 'y' | 'z',
+        quant: 'x' | 'y' | 'z',
+        fill: 'x' | 'y' | 'z'
       });
     });
   }
 });
+
+// Meteor.startup(function deck(color, shape, quant, fill) {
+//   this.color = ['r', 'b', 'g'];
+//   this.shape = ['x', 'y', 'z'];
+//   this.quant = ['1', '2', '3'];
+//   this.fill = ['empty', 'shaded', 'solid'];
+//   cards = []
+//   _.each(cards, function (name) {
+//     for (var c = 0; c < this.color.length; c++) {
+//       for (var s = 0; s < this.shape.length; s++) {
+//         for (var q = 0; q < this.quant.length; q++) {
+//           for (var f = 0; f < this.fill.length; f++) {
+//             cards.push(new card(n + 1, this.colors[c], this.shape[s], this.quant[q], this.fill[f]));
+//           }
+//         }
+//       }
+//     }
+//     Cards.insert({
+//       name: name,
+//       score: 0,
+//       color: color,
+//       shape: shape,
+//       quant: quant,
+//       fill: fill
+//
+//     });
+//   });
+// });
