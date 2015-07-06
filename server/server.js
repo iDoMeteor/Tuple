70882494382eb84b798df444bbff31d9d8632eea
Brendan Stern <bsstern@gmail.com>
2015-07-06 16:14:37 -0400

    added mongol and started building deck constructor

--- server/server.js
+++ server/server.js
@@ -1,12 +1,71 @@
-Meteor.startup(function () {
-  Cards.remove({});
-  if (Cards.find().count() === 0) {
-    var names = ["Card 1", "Card 2", "Card 3", "Card 4", "Card 5", "Card 6", "Card 7", "Card 8", "Card 9", "Card 10", "Card 11", "Card 12", "Card 13"];
-    _.each(names, function (name) {
-      Cards.insert({
-        name: name,
-        score: 0
-      });
+Meteor.startup(function deck(color, shape, quant, fill) {
+  this.color = ['r', 'b', 'g'];
+  this.shape = ['x', 'y', 'z'];
+  this.quant = ['1', '2', '3'];
+  this.fill = ['empty', 'shaded', 'solid'];
+  cards = []
+  _.each(cards, function (name) {
+    for (var c = 0; c < this.color.length; c++) {
+      for (var s = 0; s < this.shape.length; s++) {
+        for (var q = 0; q < this.quant.length; q++) {
+          for (var f = 0; f < this.fill.length; f++) {
+            cards.push(new card(n + 1, this.colors[c], this.shape[s], this.quant[q], this.fill[f]));
+          }
+        }
+      }
+    }
+    Cards.insert({
+      name: name,
+      score: 0,
+      color: color,
+      shape: shape,
+      quant: quant,
+      fill: fill
+
     });
-  }
+  });
 });
+
+
+
+// Cards.remove({});
+// if (Cards.find().count() === 0) {
+//   var names = ["Card 1", "Card 2", "Card 3", "Card 4", "Card 5", "Card 6", "Card 7", "Card 8", "Card 9", "Card 10", "Card 11", "Card 12", "Card 13"];
+//   _.each(names, function (name) {
+//     Cards.insert({
+//       name: name,
+//       score: 0
+//     });
+//   });
+// }
+
+
+// newDeck = function (deck) {
+//   deck.color = 'r' | 'b' | 'g',
+//     deck.shape = 'x' | 'y' | 'z',
+//     deck.quant = '1' | '2' | '3',
+//     deck.fill = 'empty' | 'shaded' | 'solid'
+// };
+//
+// deck = {};
+//
+// newDeck(deck);
+// deck = {
+//   color: 'r' | 'b' | 'g',
+//   shape: 'x' | 'y' | 'z',
+//   quant: '1' | '2' | '3',
+//   fill: 'empty' | 'shaded' | 'solid',
+//   createDeck: function () {
+//     return {
+//       for
+//     }
+//   }
+//
+// };
+
+// function deck(color, shape, quant, fill) {
+//   this.color = color;
+//   this.shape = shape;
+//   this.quant = quant;
+//   this.fill = fill;
+// }
