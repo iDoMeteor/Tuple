434298c59a4e95fee2dbd056e38e8a0398392f5d
Brendan Stern <bsstern@gmail.com>
2015-07-07 21:59:29 -0400

    got it working again, checkpoint

--- server/server.js
+++ server/server.js
@@ -1,72 +1,23 @@
-Cards.remove({});
-if (Cards.find().count() === 0) {
-  var names = ["Card 1", "Card 2", "Card 3", "Card 4", "Card 5", "Card 6", "Card 7", "Card 8", "Card 9", "Card 10", "Card 11", "Card 12", "Card 13"];
-  _.each(names, function (name) {
-    Cards.insert({
-      name: name,
-      score: 0,
-      color: 'x' | 'y' | 'z',
-      shape: 'x' | 'y' | 'z',
-      quant: 'x' | 'y' | 'z',
-      fill: 'x' | 'y' | 'z'
+Meteor.startup(function () {
+  Cards.remove({});
+  if (Cards.find().count() === 0) {
+    var names = ["Card 1", "Card 2", "Card 3", "Card 4", "Card 5", "Card 6", "Card 7", "Card 8", "Card 9", "Card 10", "Card 11", "Card 12", "Card 13"];
+    _.each(names, function (name) {
+      Cards.insert({
+        name: name,
+        score: 0
+          // color: 'x' | 'y' | 'z',
+          // shape: 'x' | 'y' | 'z',
+          // quant: 'x' | 'y' | 'z',
+          // fill: 'x' | 'y' | 'z'
+      });
     });
-  });
-}
+  };
+});
 
 
-// Meteor.startup(function () {
-//   // Cards.remove({});
 //
-//   function card(color, shape, quant, fill, name) {
-//     this.color = color;
-//     this.shape = shape;
-//     this.quant = quant;
-//     this.fill = fill;
-//     this.name = name;
-//   }
-//
-//   function deck() {
-//     this.color = ['red', 'green', 'blue'];
-//     this.shape = ['squiggle', 'oval', 'diamond'];
-//     this.quant = ['1', '2', '3'];
-//     this.fill = ['empty', 'striped', 'solid'];
-//     this.name = [];
-//     var cards = [];
-//     for (var c = 0; c < this.color.length; c++) {
-//       for (var s = 0; s < this.shape.length; s++) {
-//         for (var q = 0; q < this.quant.length; q++) {
-//           for (var f = 0; f < this.fill.length; f++) {
-//             Cards.push(new card(f + 1, this.color[c], this.shape[s], this.quant[q], this.fill[f]))
-//           }
-//         }
-//       }
-//     }
-//     return _.each(cards, function (card) {
-//       Cards.insert({
-//         color: cards.color,
-//         shape: cards.shape,
-//         quant: cards.quant,
-//         fill: cards.fill
-//       });
-//     });
-//   };
-// });
 
-
-
-//   if (Cards.find().count() === 0) {
-//     myDeck = new deck();
-//     _.each(myDeck, function (cards) {
-//       Cards.insert({
-//         score: 0,
-//         color: cards.color,
-//         shape: cards.shape,
-//         quant: cards.quant,
-//         fill: cards.fill
-//       });
-//     });
-//   };
-// });
 
 //cards.push(new card(f + 1, this.color[c], this.shape[s], this.quant[q], this.fill[f]))
 // reference code--> http://devdojo.com/post/create-a-deck-of-cards-in-javascript
