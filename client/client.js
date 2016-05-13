30f904119f2f28d5887a210076c55924c5c39e0b
iDoMeteor <idometeor@gmail.com>
2016-05-12 23:17:37 -0400

    Re-wrote deck generation & SVG element tags Moved deck generation to server Only generate a deck when one does not exis Added a helper Broke the card select event due to tag changes in shape1.html Added a num field to each card (first card generated is 1) Changed from function declarations to global variables for shell access Moved subscription from global to board template

--- client/client.js
+++ client/client.js
@@ -1,48 +1,74 @@
-Meteor.subscribe( "cards" );
-Template.board.helpers( {
+Template.board.helpers({
+
   cards: function () {
-    return Cards.find( {} ) ;
+    return Cards.find({}) ;
   },
+
   selectedCard: function () {
     var card = Cards.findOne( Session.get( "selectedCard" ) );
     return card && card.name;
   },
+
   cardShape: function () {
     var shape = Cards.find( Session.get( "cardShape" ) );
     return card && card.shape;
   }
-} );
-Template.board.events( {
+
+});
+
+
+Template.board.events({
+
   'click .inc': function () {
     Cards.update( Session.get( "selectedCard" ), {
       $inc: {
         score: 1
       }
-    } );
+    });
   }
-} );
-Template.card.helpers( {
-  /*** Added this ***/
+
+});
+
+
+Template.board.onCreated(function () {
+
+  Meteor.subscribe( "cards" );
+
+});
+
+
+Template.card.helpers({
+
   color: function () {
     var card = Cards.findOne( Session.get( "selectedCard" ) );
     return card && card.color;
   },
-  /*** and this ***/
+
+  num: function () {
+    var card = Cards.findOne( Session.get( "selectedCard" ) );
+    return card && card.num;
+  },
+
   shape: function () {
     var card = Cards.findOne( Session.get( "selectedCard" ) );
     return card && card.shape;
   },
-  selected: function () {
-      return Session.equals( "selectedCards", this._id ) ? "selected" : '';
-    }
-} );
 
-Template.card.events( {
+  selected: function () {
+    return Session.equals( "selectedCards", this._id ) ? "selected" : '';
+  }
+
+});
+
+
+Template.card.events({
+
   'click': function () {
     Session.set( "selectedCard", this._id );
   },
+
   'rendered': function () {
     Session.set( "cardShape", this.shape );
   }
 
-} );
+});
--- server/server.js
+++ server/server.js
@@ -1,13 +1,91 @@
-Meteor.startup( function deck() {
-  Cards.remove( {} );
-  if ( Meteor.isServer ) {
-    Meteor.publish( "cards", function () {
-      return Cards.find( {}, {
-      	limit: 81
-      } )
-    } );
+/**
+ * Object prototype
+ * */
+Card = function ( color, shape, quant, fill, name, num ) {
+  this.color = color;
+  this.shape = shape;
+  this.quant = quant;
+  this.fill = fill;
+  this.name = name;
+  this.num = num;
+  this.score = 0;
+}
+
+/**
+ * Deck generator
+ * */
+newDeck = function () {
+
+  var card = {};
+  var cards = [];
+  var i = 1;
+
+  var colors = [ '#c0f5e9', '#e2c7f3', '#e2c7f3' ];
+  var shapes = [ 'shape1', 'shape2', 'shape3' ];
+  var quants = [ 1, 2, 3 ];
+  var fills = [ 'fill1', 'fill2', 'fill3' ];
+
+
+  for ( var c = 0; c < colors.length; c++ ) {
+    for ( var s = 0; s < shapes.length; s++ ) {
+      for ( var q = 0; q < quants.length; q++ ) {
+        for ( var f = 0; f < fills.length; f++ ) {
+
+          card = new Card (
+            colors[ c ],
+            shapes[ s ],
+            quants[ q ],
+            fills[ f ],
+            colors[c]+shapes[s]+quants[q]+fills[f],
+            i
+          );
+          cards.push(card);
+
+          i++;
+
+        }
+      }
+    }
   }
-} );
+
+  return cards;
+
+}
+
+/**
+ * Insert card
+ * */
+insertDeck = function (cards) {
+
+  _.each(cards, function (card) {
+    Cards.insert(card);
+  });
+
+}
+
+/**
+ * Meteor Startup
+ * */
+Meteor.startup( function deck () {
+
+  // Check for deck
+  var deck = null;
+  if (!Cards.find().count()) {
+    deck = newDeck();
+    insertDeck(deck);
+  }
+
+  Meteor.publish( "cards", function () {
+    return Cards.find({}, {
+      limit: 12
+    });
+  });
+
+});
+
+
+
+
 // PUT IN CLIENT HELPERS?
 // Game.prototype.verifySet = function ( c0, c1, c2 ) {
 //   var s = c0.number + c1.number + c2.number;
