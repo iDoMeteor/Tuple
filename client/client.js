275b7dbad1cf04b1263b5029b67ff16513a41da0
iDoMeteor <idometeor@gmail.com>
2016-05-29 04:11:48 -0400

    Card selections working really good.. & some goodies

--- client/client.js
+++ client/client.js
@@ -28,26 +28,50 @@ Template.board.events({
 
 Template.board.onCreated(function () {
 
-  Meteor.subscribe( "cards" );
+  this.subscribe( "cards" );
 
 });
 
 
 Template.card.helpers({
 
+  active: function () {
+    return (Tuple.isSelected(this._id)) ?
+      'card-holder-active' : null;
+  },
+
   color: function () {
-    var card = Session.get( "selectedCard" );
-    return card.color;
+    return this.color;
   },
 
   num: function () {
-    var card = Session.get( "selectedCard" );
-    return card.num;
+    return this.num;
   },
 
-  shape: function () {
-    var card = Session.get( "selectedCard" );
-    return card.shape;
+  path: function () {
+    // TODO: Need to arrange shapes w/quantity > 1
+    let path  = null;
+    let paths = {
+      '1': {
+        'shape1': 'M511.9 184l-100 200 100 200 100-200',
+        'shape2': 'M587 234c-.1 0 0-.2 0-.2 0-41.4-33.6-75-75-75s-75 33.6-75 75c0 .9.1 1.7.1 2.6v294.9c0 .9-.1 1.7-.1 2.6 0 41.4 33.6 75 75 75 41.3 0 74.9-33.8 75-74.8h.1V234z',
+        'shape3': 'M627.7 234c-.1 0-.1-.2-.1-.2-5.5-41.4-75.2-75-116.7-75-41.4 0-38.8 33.6-33.3 75 .1.9.3 1.7.5 2.6 14.2 98.3-98.3 196.6-84.2 294.9.1.9.2 1.7.3 2.6 5.5 41.4 75.2 75 116.7 75 41.2 0 38.7-33.8 33.4-74.8h.1C527.5 434 644.2 334 627.7 234z',
+      },
+      '2': {
+        'shape1': 'M511.9 184l-100 200 100 200 100-200',
+        'shape2': 'M587 234c-.1 0 0-.2 0-.2 0-41.4-33.6-75-75-75s-75 33.6-75 75c0 .9.1 1.7.1 2.6v294.9c0 .9-.1 1.7-.1 2.6 0 41.4 33.6 75 75 75 41.3 0 74.9-33.8 75-74.8h.1V234z',
+        'shape3': 'M627.7 234c-.1 0-.1-.2-.1-.2-5.5-41.4-75.2-75-116.7-75-41.4 0-38.8 33.6-33.3 75 .1.9.3 1.7.5 2.6 14.2 98.3-98.3 196.6-84.2 294.9.1.9.2 1.7.3 2.6 5.5 41.4 75.2 75 116.7 75 41.2 0 38.7-33.8 33.4-74.8h.1C527.5 434 644.2 334 627.7 234z',
+      },
+      '3': {
+        'shape1': 'M511.9 184l-100 200 100 200 100-200',
+        'shape2': 'M587 234c-.1 0 0-.2 0-.2 0-41.4-33.6-75-75-75s-75 33.6-75 75c0 .9.1 1.7.1 2.6v294.9c0 .9-.1 1.7-.1 2.6 0 41.4 33.6 75 75 75 41.3 0 74.9-33.8 75-74.8h.1V234z',
+        'shape3': 'M627.7 234c-.1 0-.1-.2-.1-.2-5.5-41.4-75.2-75-116.7-75-41.4 0-38.8 33.6-33.3 75 .1.9.3 1.7.5 2.6 14.2 98.3-98.3 196.6-84.2 294.9.1.9.2 1.7.3 2.6 5.5 41.4 75.2 75 116.7 75 41.2 0 38.7-33.8 33.4-74.8h.1C527.5 434 644.2 334 627.7 234z',
+      },
+    }
+    let qty = this.quant.toString();
+    let shape = this.shape;
+    path = paths[qty][shape];
+    return path;
   },
 
   selected: function () {
@@ -71,6 +95,16 @@ Template.card.events({
   'rendered': function () {
     Session.set( "cardShape", this.shape );
   }
+
+});
+
+
+Template.card.onRendered(function () {
+
+  let color = this.data.color;
+  let id = this.data._id;
+  let num = this.data.num;
+  this.$('#svg-' + num).attr('style', 'fill:' + color);
 
 });
 
--- server/server.js
+++ server/server.js
@@ -1,79 +1,13 @@
 /**
- * Object prototype
- * */
-Card = function ( color, shape, quant, fill, name, num ) {
-  this.color = color;
-  this.shape = shape;
-  this.quant = quant;
-  this.fill = fill;
-  this.name = name;
-  this.num = num;
-  this.score = 0;
-}
-
-/**
- * Deck generator
- * */
-newDeck = function () {
-
-  var card = {};
-  var cards = [];
-  var i = 1;
-
-  var colors = [ '#c0f5e9', '#e2c7f3', '#e2c7f3' ];
-  var shapes = [ 'shape1', 'shape2', 'shape3' ];
-  var quants = [ 1, 2, 3 ];
-  var fills = [ 'fill1', 'fill2', 'fill3' ];
-
-
-  for ( var c = 0; c < colors.length; c++ ) {
-    for ( var s = 0; s < shapes.length; s++ ) {
-      for ( var q = 0; q < quants.length; q++ ) {
-        for ( var f = 0; f < fills.length; f++ ) {
-
-          card = new Card (
-            colors[ c ],
-            shapes[ s ],
-            quants[ q ],
-            fills[ f ],
-            colors[c]+shapes[s]+quants[q]+fills[f],
-            i
-          );
-          cards.push(card);
-
-          i++;
-
-        }
-      }
-    }
-  }
-
-  return cards;
-
-}
-
-/**
- * Insert card
- * */
-insertDeck = function (cards) {
-
-  _.each(cards, function (card) {
-    Cards.insert(card);
-  });
-
-}
-
-/**
  * Meteor Startup
  * */
 Meteor.startup( function deck () {
 
   // Check for deck
-  var deck = null;
   if (!Cards.find().count()) {
     console.log('Tuple: Generating initial deck');
-    deck = newDeck();
-    insertDeck(deck);
+    let deck = Tuple.newDeck();
+    Tuple.insertDeck(deck);
   }
 
   Meteor.publish( "cards", function () {
@@ -83,79 +17,3 @@ Meteor.startup( function deck () {
   });
 
 });
-
-
-
-
-// PUT IN CLIENT HELPERS?
-// Game.prototype.verifySet = function ( c0, c1, c2 ) {
-//   var s = c0.number + c1.number + c2.number;
-//   if ( s != 0 && s != 3 && s != 6 ) return false;
-//   s = c0.color + c1.color + c2.color;
-//   if ( s != 0 && s != 3 && s != 6 ) return false;
-//   s = c0.shape + c1.shape + c2.shape;
-//   if ( s != 0 && s != 3 && s != 6 ) return false;
-//   s = c0.shading + c1.shading + c2.shading;
-//   if ( s != 0 && s != 3 && s != 6 ) return false;
-//   return true;
-// }
-// NEED TO APPLY THIS IDX INDEX TO MY CARD LOGIC
-// function Card( idx ) {
-//   this.number = idx % 3;
-//   idx = Math.floor( idx / 3 );
-//   this.color = idx % 3;
-//   idx = Math.floor( idx / 3 );
-//   this.shape = idx % 3;
-//   idx = Math.floor( idx / 3 );
-//   this.shading = idx % 3;
-// }
-//
-//
-//
-// reference code--> http://devdojo.com/post/create-a-deck-of-cards-in-javascript
-// function card(value, name, suit) {
-//   this.value = value;
-//   this.name = name;
-//   this.suit = suit;
-// }
-//
-// function deck() {
-//   this.names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-//   this.suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
-//   var cards = [];
-//
-//   for (var s = 0; s < this.suits.length; s++) {
-//     for (var n = 0; n < this.names.length; n++) {
-//       cards.push(new card(n + 1, this.names[n], this.suits[s]));
-//     }
-//   }
-//
-//   return cards;
-// }
-// var myDeck = new deck();
-// console.log(myDeck);
-//
-// window.onload = function() {
-//
-// 	for(var i=0; i < myDeck.length; i++){
-// 		div = document.createElement('div');
-// 		div.className = 'card';
-//
-// 		if(myDeck[i].suit == 'Diamonds'){
-// 			var ascii_char = '♦';
-// 		} else {
-// 			var ascii_char = '&' + myDeck[i].suit.toLowerCase() + ';';
-// 		}
-//
-// 		div.innerHTML = '' + myDeck[i].name + '' + ascii_char + '';
-// 		document.body.appendChild(div);
-// 	}
-//
-// }
-//
-// function shuffle(o) {
-// 	for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-// 	return o;
-// };
-//
-// myDeck = shuffle(myDeck);
